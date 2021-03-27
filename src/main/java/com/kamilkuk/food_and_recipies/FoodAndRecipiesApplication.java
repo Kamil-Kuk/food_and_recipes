c7045e4eeb16123ad4169b7f103a7453c02683fd
kamilK <kamil.kukowski@gmail.com>
2021-03-27 12:13:20 +0100

    initial commit

---
+++ src/main/java/com/kamilkuk/food_and_recipies/FoodAndRecipiesApplication.java
@@ -0,0 +1,13 @@
+package com.kamilkuk.food_and_recipies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodAndRecipiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodAndRecipiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kamilkuk/food_and_recipies/FoodAndRecipiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamilkuk.food_and_recipies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodAndRecipiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
