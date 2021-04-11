6aded9a0a57c91a2e97b58f258ad9d269b0b10f5
kamilK <kamil.kukowski@gmail.com>
2021-04-11 21:30:23 +0200

    feat[service]: added methods for ProductService

--- src/main/java/com/kamilkuk/food_and_recipes/service/RecipeService.java
+++ src/main/java/com/kamilkuk/food_and_recipes/service/RecipeService.java
@@ -6,6 +6,7 @@ import com.kamilkuk.food_and_recipes.repository.RecipeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 
@@ -14,7 +15,6 @@ import java.util.NoSuchElementException;
 public class RecipeService {
 
     private final RecipeRepository recipeRepository;
-    private final ProductRepository productRepository;
 
     public Recipe get(Long id){
         return recipeRepository.findById(id).orElseThrow(
@@ -25,4 +25,21 @@ public class RecipeService {
         return recipeRepository.save(recipe);
     }
 
+    public List<Recipe> getAll(){
+        return recipeRepository.findAll();
+    }
+
+    public Recipe update(Recipe recipe){
+        return recipeRepository.save(recipe);
+    }
+
+    public boolean remove(Long id){
+        recipeRepository.delete(get(id));
+        try{
+            get(id);
+            return false;
+        }catch (NoSuchElementException e){
+            return true;
+        }
+    }
 }
---
+++ src/test/java/com/kamilkuk/food_and_recipes/service/RecipeServiceTest.java
@@ -0,0 +1,61 @@
+package com.kamilkuk.food_and_recipes.service;
+
+import com.kamilkuk.food_and_recipes.model.Recipe;
+import com.kamilkuk.food_and_recipes.repository.RecipeRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+
+@ExtendWith(MockitoExtension.class)
+class RecipeServiceTest {
+
+    @Mock
+    private RecipeRepository repositoryMock;
+
+    private static Recipe recipe1;
+    private static Recipe recipe2;
+    private static Recipe recipe3;
+
+    @BeforeAll
+    static void setUp(){
+        recipe1 = new Recipe();
+        recipe1.setId(1001L);
+        recipe2 = new Recipe();
+        recipe2.setId(1002L);
+        recipe3 = new Recipe();
+        recipe3.setId(1003L);
+    }
+
+    @Test
+    void should_save_recipe(){
+        //given
+        RecipeService service = new RecipeService(repositoryMock);
+
+        //when
+        service.save(recipe1);
+
+        //then
+        Mockito.verify(repositoryMock).save(recipe1);
+    }
+
+//    @Test
+//    void should_return_true_on_successful_delete(){
+//        //given
+//        RecipeService service = new RecipeService(repositoryMock);
+//        Mockito.when(repositoryMock.findById(1001L)).thenReturn(Optional.of(recipe1));
+//
+//        //when
+//        boolean result = service.remove(1001L);
+//
+//        //then
+//        Assertions.assertTrue(result);
+//    }
+
+}
